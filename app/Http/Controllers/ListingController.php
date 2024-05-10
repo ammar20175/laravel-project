1d2f4e120b22c1254a8fe396d5a4b9278da8f633
ammar ahmad <ammar.ahmad6410@gmail.com>
2024-05-10 19:20:55 +0400

    add manage listings plus authorization

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -44,6 +44,8 @@ class ListingController extends Controller
             $formFields['logo'] = $request->file('logo')->store('logos', 'public');
         }
 
+        $formFields['user_id'] = auth()->id();
+
         Listing::create($formFields);
 
         return redirect('/')->with('message', 'job listing created successfully.');
@@ -56,6 +58,10 @@ class ListingController extends Controller
 
     public function update(Request $request, Listing $listing)
     {
+        if ($listing->user_id != auth()->id()) {
+            abort(403, 'Unauthorized Action');
+        }
+
         $formFields = $request->validate([
             'title' => 'required',
             'company' => ['required'],
@@ -77,7 +83,16 @@ class ListingController extends Controller
 
     public function destroy(Listing $listing)
     {
+        if ($listing->user_id != auth()->id()) {
+            abort(403, 'Unauthorized Action');
+        }
+
         $listing->delete();
         return redirect('/')->with('message', 'Listing deleted successfully');
+    }
+
+    public function manage()
+    {
+        return view('listings.manage', ['listings' => auth()->user()->listings]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,8 @@ Route::put('/listings/{listing}', [ListingController::class, 'update'])->middlew
 
 Route::delete('/listings/{listing}', [ListingController::class, 'destroy'])->middleware('auth');
 
+Route::get('/listings/manage', [ListingController::class, 'manage'])->middleware('auth');;
+
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
 
 // users routes
